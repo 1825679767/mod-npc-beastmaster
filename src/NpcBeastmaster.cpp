83a1d0ed52e15bb24646ca32de88480b8f95cf75
1825679767 <124947689+1825679767@users.noreply.github.com>
2024-05-29 15:21:19 +0800

    删除遗忘技能选择

--- src/NpcBeastmaster.cpp
+++ src/NpcBeastmaster.cpp
@@ -170,8 +170,8 @@ void NpcBeastmaster::GossipSelect(Player* player, Creature* creature, uint32 act
         }
 
         // remove pet skills (not for hunters)
-        if (player->getClass() != CLASS_HUNTER && player->HasSpell(PET_SPELL_CALL_PET))
-            AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "遗忘猎人技能", GOSSIP_SENDER_MAIN, PET_REMOVE_SKILLS);
+        //  if (player->getClass() != CLASS_HUNTER && player->HasSpell(PET_SPELL_CALL_PET))
+        //    AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "遗忘猎人技能", GOSSIP_SENDER_MAIN, PET_REMOVE_SKILLS);
 
         // Stables for hunters only - Doesn't seem to work for other classes
         if (player->getClass() == CLASS_HUNTER)
